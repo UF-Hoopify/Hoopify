654a9a214287cad7ad9fa6ee2ee073d1ba39bd4e
saiNaruUFL <saichandunaru@gmail.com>
2026-01-22 17:35:38 -0500

    feat(types): add additional properties to CourtDetails interface

--- src/types/CourtSearchTypes.ts
+++ src/types/CourtSearchTypes.ts
@@ -6,3 +6,11 @@ export interface Court {
   address: string;
   available: number;
 }
+
+export interface CourtDetails extends Court {
+  photos?: string[];
+  rating?: number;
+  totalRatings?: number;
+  isOpenNow?: boolean;
+  closingTime?: string;
+}
